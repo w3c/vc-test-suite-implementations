3807b62e5c16b0bb36c48145191f6b483a15088f
Andrew Jones <ajones@digitalbazaar.com>
2022-06-10 10:48:38 -0400

    Use allImplementations instead of implementations.

--- test/Example.spec.js
+++ test/Example.spec.js
@@ -4,10 +4,10 @@
 import chai from 'chai';
 const should = chai.should();
 
-import {implementations} from '../lib/main.js';
+import {allImplementations} from '../lib/main.js';
 
 describe('implementations', () => {
   it('should exist', async () => {
-    should.exist(implementations);
+    should.exist(allImplementations);
   });
 });
