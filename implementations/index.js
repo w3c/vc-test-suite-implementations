89c220f57725947c4fba4743a26c9f9d163ba913
Andrew Jones <ajones@digitalbazaar.com>
2024-03-28 16:12:25 +0000

    Only support localConfig.cjs.

--- implementations/index.js
+++ implementations/index.js
@@ -28,19 +28,9 @@ const getLocalManifest = fileName => {
   }
 };
 
-// get all local endpoints dot file or otherwise
-manifests.local = [
-  '.localImplementationsConfig.cjs',
-  'localImplementationsConfig.cjs'
-].flatMap(path => getLocalManifest(path));
-
-// get localConfig and look for a manifest
-const localConfig = [
-  '.localConfig.cjs',
-  'localConfig.cjs'
-].flatMap(path => getLocalManifest(path)?.implementations);
-// add localConfig manifests to existings manifests.local
-manifests.local.concat(localConfig);
+// get localConfig and look for an implementations property
+const local = manifests.local = getLocalManifest('localConfig.cjs')?.
+  implementations || [];
 
 // concat all the implementation manifests together
 const all = manifests.all = manifests.remote.concat(manifests.local);
@@ -48,4 +38,5 @@ const all = manifests.all = manifests.remote.concat(manifests.local);
 // look for only in a local manifests
 const only = manifests.only = manifests.local.filter(i => i?.only === true);
 
-export const implementerFiles = only.length ? only : all;
+// if local implementations are defined only return local implementations
+export const implementerFiles = local.length ? only : all;
